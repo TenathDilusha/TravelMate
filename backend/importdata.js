17759d6267f500e4940d8664ddc7c9ae7771914f
Tenath Dilusha <tenathdilusha@gmail.com>
2026-02-20 23:06:39 +0530

    created a database

--- backend/importdata.js
+++ backend/importdata.js
@@ -4,11 +4,12 @@ const { Pool } = require("pg");
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.RENDER_DB_HOST,
+  user: process.env.RENDER_DB_USER,
+  password: process.env.RENDER_DB_PASSWORD,
+  database: process.env.RENDER_DB_NAME,
+  port: process.env.RENDER_DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 const locationsMap = {};
